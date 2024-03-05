4a7d889d47ec1b3fce8b72d34b988155f187e906
chaos2171053 <chaos2171053@gmail.com>
2024-03-05 10:55:10 +0800

    leetcode:linked list

--- leetcode/src/main/java/org/example/LinkedList/IntersectionOfTwoLinkedLists.java
+++ leetcode/src/main/java/org/example/LinkedList/IntersectionOfTwoLinkedLists.java
@@ -5,44 +5,23 @@ package org.example.LinkedList;
 // 如果两个链表没有交点，返回 null 。
 public class IntersectionOfTwoLinkedLists {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode curA = headA;
-        ListNode curB = headB;
-        int lenA = 0, lenB = 0;
-        while (curA != null) { // 求链表A的长度
-            lenA++;
-            curA = curA.next;
-        }
-        while (curB != null) { // 求链表B的长度
-            lenB++;
-            curB = curB.next;
-        }
-        curA = headA;
-        curB = headB;
-        // 让curA为最长链表的头，lenA为其长度
-        if (lenB > lenA) {
-            //1. swap (lenA, lenB);
-            int tmpLen = lenA;
-            lenA = lenB;
-            lenB = tmpLen;
-            //2. swap (curA, curB);
-            ListNode tmpNode = curA;
-            curA = curB;
-            curB = tmpNode;
-        }
-        // 求长度差
-        int gap = lenA - lenB;
-        // 让curA和curB在同一起点上（末尾位置对齐）
-        while (gap-- > 0) {
-            curA = curA.next;
-        }
-        // 遍历curA 和 curB，遇到相同则直接返回
-        while (curA != null) {
-            if (curA == curB) {
-                return curA;
+        // p1 指向 A 链表头结点，p2 指向 B 链表头结点
+        ListNode p1 = headA, p2 = headB;
+        while (p1 != p2) {
+            // p1 走一步，如果走到 A 链表末尾，转到 B 链表
+            if (p1 == null) {
+                p1 = headB;
+            } else {
+                p1 = p1.next;
             }
-            curA = curA.next;
-            curB = curB.next;
+            // p2 走一步，如果走到 B 链表末尾，转到 A 链表
+            if (p2 == null) {
+                p2 = headA;
+            } else {
+                p2 = p2.next;
+            }
         }
-        return null;
+        return p1;
+
     }
 }
